39da2f1a68dc02ee565177334f876b188f018440
Jacob Daniels-Flechtner <jatdanie@ucsc.edu>
2019-11-12 22:47:17 -0800

    Fixed a bug

--- phaserDnD/myGame/js/main.js
+++ phaserDnD/myGame/js/main.js
@@ -20,6 +20,8 @@ window.onload = function() {
 
 var MAIN_FONT = 'Verdana'; // main font
 var MAIN_STYLE = 'bold'; // main font style
+var BUTTON_WIDTH = 193;
+var BUTTON_HEIGHT = 71;
 
 var PROPERTIES =
 {
@@ -65,11 +67,17 @@ Start.prototype = {
 		game.load.spritesheet('button', 'assets/img/button_sprite_sheet2.png', 193, 71);
 	},
 	
-	
 	create: function() 
 	{
 		game.stage.backgroundColor = '#000000';
-
+		
+		for (var key in PROPERTIES.CLASS_BUCKETS)
+		{
+			PROPERTIES.CLASS_BUCKETS[key] = 0;
+		}
+		
+		//console.log(PROPERTIES.CLASS_BUCKETS);
+		
 		function onUp(button, pointer, isOver)
 		{
 			if (isOver)
@@ -86,6 +94,8 @@ Start.prototype = {
 		buttonText = game.add.text(game.world.centerX, game.world.centerY + 3, 'Start Quiz', { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '20px', fill: '#ffffff', align: "center" });
 		buttonText.anchor.x = 0.5;
 		buttonText.anchor.y = 0.5;
+		
+		button.scale.setTo((buttonText.width/BUTTON_WIDTH) + 0.15, (buttonText.height/BUTTON_HEIGHT) + 0.3);
 	},
 	
 	update: function() 
@@ -105,7 +115,7 @@ Question.prototype = {
 	create: function()
 	{
 		QVal = "Q" + PROPERTIES.QUESTION;
-		console.log(QVal);
+		//console.log(QVal);
 		
 		QData = CLASS_QUESTIONS[QVal];
 		Q = QData.Q;
@@ -114,39 +124,181 @@ Question.prototype = {
 		QText.anchor.x = 0.5;
 		QText.anchor.y = 0.5;
 		
-		
-		for (var i = 0; i < Object.keys(QData.A).length; i++)
+		if (Object.keys(QData.A).length >= 1)
 		{
-			function onUp(button, pointer, isOver)
+			function onUp1(button, pointer, isOver)
 			{
 				if (isOver)
 				{
-					console.log(PROPERTIES.CLASS_BUCKETS);
-					//console.log(QData.A[i].REWARD);
-					console.log(QData.A[i]);
-					var rewards = QData.A[i].REWARD;
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					//console.log(QData.A[0].REWARD);
+					//console.log(QData.A[0]);
+					var rewards = QData.A[0].REWARD;
 					
 					for (var k = 0; k < rewards.length; k++)
 					{
-						console.log(rewards[k]);
+						//console.log(rewards[k]);
 						PROPERTIES.CLASS_BUCKETS[rewards[k]]++;
 					}
 					
-					console.log(PROPERTIES.CLASS_BUCKETS);
+					//console.log(PROPERTIES.CLASS_BUCKETS);
 					
 					PROPERTIES.QUESTION = 0;
 					game.state.start('Start');
 				}
 			}
 			
-			var AButton = game.add.button(game.world.centerX, 225 + (75 * i), 'button', onUp, this, 2, 1, 0);
-			AButton.anchor.x = 0.5;
-			AButton.anchor.y = 0.5;
+			var AButton1 = game.add.button(game.world.centerX, 225 + (75 * 0), 'button', onUp1, this, 2, 1, 0);
+			AButton1.anchor.x = 0.5;
+			AButton1.anchor.y = 0.5;
 			
-			var AText = game.add.text(game.world.centerX, 225 + (75 * i), QData.A[i].TEXT, { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '18px', fill: '#ffffff', align: "center" });
-			AText.anchor.x = 0.5;
-			AText.anchor.y = 0.5;
+			var AText1 = game.add.text(game.world.centerX, 225 + (75 * 0) + 3, QData.A[0].TEXT, { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '18px', fill: '#ffffff', align: "center" });
+			AText1.anchor.x = 0.5;
+			AText1.anchor.y = 0.5;
+			
+			AButton1.scale.setTo((AText1.width/BUTTON_WIDTH) + 0.15, (AText1.height/BUTTON_HEIGHT) + 0.3);
 		}
+		
+		if (Object.keys(QData.A).length >= 2)
+		{
+			function onUp2(button, pointer, isOver)
+			{
+				if (isOver)
+				{
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					//console.log(QData.A[1].REWARD);
+					//console.log(QData.A[1]);
+					var rewards = QData.A[1].REWARD;
+					
+					for (var k = 0; k < rewards.length; k++)
+					{
+						//console.log(rewards[k]);
+						PROPERTIES.CLASS_BUCKETS[rewards[k]]++;
+					}
+					
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					
+					PROPERTIES.QUESTION = 0;
+					game.state.start('Start');
+				}
+			}
+			
+			var AButton2 = game.add.button(game.world.centerX, 225 + (75 * 1), 'button', onUp2, this, 2, 1, 0);
+			AButton2.anchor.x = 0.5;
+			AButton2.anchor.y = 0.5;
+			
+			var AText2 = game.add.text(game.world.centerX, 225 + (75 * 1) + 3, QData.A[1].TEXT, { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '18px', fill: '#ffffff', align: "center" });
+			AText2.anchor.x = 0.5;
+			AText2.anchor.y = 0.5;
+			
+			AButton2.scale.setTo((AText2.width/BUTTON_WIDTH) + 0.15, (AText2.height/BUTTON_HEIGHT) + 0.3);
+		}
+		
+		if (Object.keys(QData.A).length >= 3)
+		{
+			function onUp3(button, pointer, isOver)
+			{
+				if (isOver)
+				{
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					//console.log(QData.A[i].REWARD);
+					//console.log(QData.A[2]);
+					var rewards = QData.A[2].REWARD;
+					
+					for (var k = 0; k < rewards.length; k++)
+					{
+						//console.log(rewards[k]);
+						PROPERTIES.CLASS_BUCKETS[rewards[k]]++;
+					}
+					
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					
+					PROPERTIES.QUESTION = 0;
+					game.state.start('Start');
+				}
+			}
+			
+			var AButton3 = game.add.button(game.world.centerX, 225 + (75 * 2), 'button', onUp3, this, 2, 1, 0);
+			AButton3.anchor.x = 0.5;
+			AButton3.anchor.y = 0.5;
+			
+			var AText3 = game.add.text(game.world.centerX, 225 + (75 * 2) + 3, QData.A[2].TEXT, { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '18px', fill: '#ffffff', align: "center" });
+			AText3.anchor.x = 0.5;
+			AText3.anchor.y = 0.5;
+			
+			AButton3.scale.setTo((AText3.width/BUTTON_WIDTH) + 0.15, (AText3.height/BUTTON_HEIGHT) + 0.3);
+		}
+		
+		if (Object.keys(QData.A).length >= 4)
+		{
+			function onUp4(button, pointer, isOver)
+			{
+				if (isOver)
+				{
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					//console.log(QData.A[3].REWARD);
+					//console.log(QData.A[3]);
+					var rewards = QData.A[3].REWARD;
+					
+					for (var k = 0; k < rewards.length; k++)
+					{
+						//console.log(rewards[k]);
+						PROPERTIES.CLASS_BUCKETS[rewards[k]]++;
+					}
+					
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					
+					PROPERTIES.QUESTION = 0;
+					game.state.start('Start');
+				}
+			}
+			
+			var AButton4 = game.add.button(game.world.centerX, 225 + (75 * 3), 'button', onUp4, this, 2, 1, 0);
+			AButton4.anchor.x = 0.5;
+			AButton4.anchor.y = 0.5;
+			
+			var AText4 = game.add.text(game.world.centerX, 225 + (75 * 3), QData.A[3].TEXT, { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '18px', fill: '#ffffff', align: "center" });
+			AText4.anchor.x = 0.5;
+			AText4.anchor.y = 0.5;
+			
+			AButton4.scale.setTo((AText4.width/BUTTON_WIDTH) + 0.15, (AText4.height/BUTTON_HEIGHT) + 0.3);
+		}
+		
+		if (Object.keys(QData.A).length >= 5)
+		{
+			function onUp5(button, pointer, isOver)
+			{
+				if (isOver)
+				{
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					//console.log(QData.A[4].REWARD);
+					//console.log(QData.A[4]);
+					var rewards = QData.A[4].REWARD;
+					
+					for (var k = 0; k < rewards.length; k++)
+					{
+						//console.log(rewards[k]);
+						PROPERTIES.CLASS_BUCKETS[rewards[k]]++;
+					}
+					
+					//console.log(PROPERTIES.CLASS_BUCKETS);
+					
+					PROPERTIES.QUESTION = 0;
+					game.state.start('Start');
+				}
+			}
+			
+			var AButton5 = game.add.button(game.world.centerX, 225 + (75 * 4), 'button', onUp5, this, 2, 1, 0);
+			AButton5.anchor.x = 0.5;
+			AButton5.anchor.y = 0.5;
+			
+			var AText5 = game.add.text(game.world.centerX, 225 + (75 * 4) + 3, QData.A[4].TEXT, { font: MAIN_FONT, fontStyle: MAIN_STYLE, fontSize: '18px', fill: '#ffffff', align: "center" });
+			AText5.anchor.x = 0.5;
+			AText5.anchor.y = 0.5;
+			
+			AButton5.scale.setTo((AText5.width/BUTTON_WIDTH) + 0.15, (AText5.height/BUTTON_HEIGHT) + 0.3);
+		}
+		
 	},
 	
 	update: function()
