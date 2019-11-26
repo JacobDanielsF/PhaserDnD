af71349a3a988580b62d14885dc3054178eb24cd
Jacob Daniels-Flechtner <jatdanie@ucsc.edu>
2019-11-26 11:16:26 -0800

    Added hyperlink to survey

--- phaserDnD/myGame/js/States/End.js
+++ phaserDnD/myGame/js/States/End.js
@@ -5,8 +5,16 @@ End.prototype = {
 		
 	},
 	
+	onUpSurvey: function(button, pointer, isOver)
+	{
+		if (isOver)
+		{
+			window.open("https://forms.gle/JF27D4gyDng3nSjQ6", "_blank");
+		}
+	},
+	
 	//for button to move to next state
-	onUp: function(button, pointer, isOver)
+	onUpQuizReset: function(button, pointer, isOver)
 	{
 		if (isOver)
 		{
@@ -26,24 +34,37 @@ End.prototype = {
 	create: function() 
 	{
 		//add title text
-		title = game.add.text(game.world.centerX, 175, "Your results:", titleTextStyle);
+		title = game.add.text(game.world.centerX, 165, "Your results:", titleTextStyle);
 		title.anchor.x = 0.5;
 		title.anchor.y = 0.5;
 		
-		classText = game.add.text(game.world.centerX, 280, "Class: " + PROPERTIES.CLASS, textStyle);
+		classText = game.add.text(game.world.centerX, 270, "Class: " + PROPERTIES.CLASS, textStyle);
 		classText.anchor.x = 0.5;
 		classText.anchor.y = 0.5;
 		
-		raceText = game.add.text(game.world.centerX, 340, "Race: " + PROPERTIES.RACE, textStyle);
+		raceText = game.add.text(game.world.centerX, 330, "Race: " + PROPERTIES.RACE, textStyle);
 		raceText.anchor.x = 0.5;
 		raceText.anchor.y = 0.5;
 		
-		var button = game.add.button(game.world.centerX, standardTitleHeight + (100 * 3.25), 'button', this.onUp, this, 2, 1, 0);
+		
+		var surveyButton = game.add.button(game.world.centerX, standardTitleHeight + (100 * 2.75), 'button', this.onUpSurvey, this, 2, 1, 0);
+		surveyButton.anchor.x = 0.5;
+		surveyButton.anchor.y = 0.5;
+		surveyButton.alpha = .5;
+		
+		var surveyText = game.add.text(game.world.centerX, standardTitleHeight + (100 * 2.75) + 4, "Please fill out our survey!", titleTextStyle);
+		surveyText.anchor.x = 0.5;
+		surveyText.anchor.y = 0.5;
+		
+		surveyButton.scale.setTo((surveyText.width/BUTTON_WIDTH) + 0.15, (surveyText.height/BUTTON_HEIGHT) + 0.3);
+		
+		
+		var button = game.add.button(game.world.centerX, standardTitleHeight + (100 * 3.75), 'button', this.onUpQuizReset, this, 2, 1, 0);
 		button.anchor.x = 0.5;
 		button.anchor.y = 0.5;
 		button.alpha = .5;
 		
-		var text = game.add.text(game.world.centerX, standardTitleHeight + (100 * 3.25) + 4, "Restart quiz", titleTextStyle);
+		var text = game.add.text(game.world.centerX, standardTitleHeight + (100 * 3.75) + 4, "Restart quiz", titleTextStyle);
 		text.anchor.x = 0.5;
 		text.anchor.y = 0.5;
 		
