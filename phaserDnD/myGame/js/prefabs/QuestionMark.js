2a3df451c5bfc0f7994060a84302f512e2352773
Paula <pnavarr1@ucsc.edu>
2019-12-07 22:37:30 -0800

    Fixed overlap bug with tooltip text

--- phaserDnD/myGame/js/prefabs/QuestionMark.js
+++ phaserDnD/myGame/js/prefabs/QuestionMark.js
@@ -5,38 +5,37 @@ function QuestionMark(x, y, key, text, game) {
 	Phaser.Sprite.call(this, game, x, y, key);
 
     this.inputEnabled = true;
-    this.text = game.add.group();
     this.game = game; 
 
+    //use for keeping track in Update()
+    this.pointerOver = false; 
+
     //add textbox image
-    var backRect = game.add.image(x + 35, y + 35, 'textBox');
-    this.text.add(backRect)
+    this.backRect = game.add.image(x + 35, y + 35, 'textBox');
 
     //add text
-    var blurbText = game.add.text(x + 45, y + 45, text, textStyle);
-    blurbText.fontSize = 18;
-    blurbText.fill = '#EEE8FC';
+    this.blurbText = game.add.text(x + 45, y + 45, text, textStyle);
+    this.blurbText.fontSize = 18;
+    this.blurbText.fill = '#EEE8FC';
 
     //adjust textbox size to text
-	backRect.width = blurbText.width + 20;
-	backRect.height = blurbText.height + 15;
-	backRect.alpha = .95;
+	this.backRect.width = this.blurbText.width + 20;
+	this.backRect.height = this.blurbText.height + 15;
+	this.backRect.alpha = .95;
 
-	//group it!!
-    this.text.add(blurbText);
-    this.text.alpha = 0;
+    //initialize
+    this.backRect.alpha = 0;
+    this.blurbText.alpha = 0;
 
     //make sure text is always on screen
-    if (backRect.x + backRect.width > game.width) {
-    	blurbText.x = game.width - blurbText.width - 20;
-    	backRect.x = game.width - backRect.width - 10;
+    if (this.backRect.x + this.backRect.width > game.width) {
+    	this.blurbText.x = game.width - this.blurbText.width - 20;
+    	this.backRect.x = game.width - this.backRect.width - 10;
     }
-    if (backRect.y + backRect.height > game.height) {
-        blurbText.y = game.height - blurbText.height - 20;
-        backRect.y = game.height - backRect.height - 10;
+    if (this.backRect.y + this.backRect.height > game.height) {
+        this.blurbText.y = game.height - this.blurbText.height - 20;
+        this.backRect.y = game.height - this.backRect.height - 10;
     }
-
-    // questionMarkGroup.add(this);
 }
 
 QuestionMark.prototype = Object.create(Phaser.Sprite.prototype);
@@ -45,11 +44,23 @@ QuestionMark.prototype.constructor = QuestionMark;
 QuestionMark.prototype.update = function() {
     if (this.input.pointerOver())
     {
-        this.text.alpha = 1;
+        this.backRect.alpha = .8;
+        this.blurbText.alpha = 1;
+
+        //prevent flickering 
+        if (!this.pointerOver) {
+            this.backRect.bringToTop();
+            this.blurbText.bringToTop();
+        }
+
+        this.pointerOver = true; 
     }
     else
     {
-    	this.text.alpha = 0;
+    	this.backRect.alpha = 0;
+        this.blurbText.alpha = 0;
+
+        this.pointerOver = false; 
     }
 
 }
